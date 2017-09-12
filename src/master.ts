4a6349f1f6bb09b8c1297669b1c437571038250b
Alexey Sirotkin <asirotkin@changers.team>
2017-09-12 09:41:28 +0300

    added express server

--- src/master.ts
+++ src/master.ts
@@ -1,11 +1,26 @@
+import * as express from 'express';
+import * as http from 'http';
+import * as url from 'url';
 import * as WebSocket from 'ws';
 
-const wss = new WebSocket.Server({ port: 8080 });
 
-wss.on('connection', function connection(ws) {
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+
+wss.on('connection', function connection(ws, req) {
+  app.use(function (req, res) {
+    ws.send('req');
+    res.send({ msg: "hello" });
+  });
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
 
   ws.send('something');
+});
+
+server.listen(8080, function listening() {
+  console.log('Listening on %d', server.address().port);
 });
